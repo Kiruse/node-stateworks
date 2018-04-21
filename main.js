67be31ad3983bde6efa12e126a667a2864289b2b
Kiruse <26940742+Kiruse@users.noreply.github.com>
2018-04-21 19:43:45 +0200

    Added common properties shared across all states of the Stateful.

--- main.js
+++ main.js
@@ -27,12 +27,20 @@ Object.assign(Stateful.prototype, {
     proxy: null, // Filled during construction.
     scope: null,
     states: {},
+    common: {}, // Properties shared across states. These have highest priority.
     currentState: undefined,
     defaultState: undefined,
     transitionHandler (){},
     
     bind (scope) {
         this.scope = scope;
+        return this;
+    },
+    
+    setCommon (properties) {
+        if (typeof properties === 'object') {
+            Object.assign(this.common, properties);
+        }
         return this;
     },
     
@@ -86,40 +94,60 @@ const traps = {
         return Reflect.defineProperty(this.stateful.states[this.stateful.currentState], key, descriptor);
     },
     getOwnPropertyDescriptor (target, key) {
+        if (key in this.stateful.common) {
+            return Reflect.getOwnPropertyDescriptor(this.stateful.common, key);
+        }
         return Reflect.getOwnPropertyDescriptor(this.stateful.states[this.stateful.currentState], key);
     },
     has (target, key) {
-        return key in this.stateful.states[this.stateful.currentState] || key in this.stateful.states[this.stateful.defaultState];
+        return key in this.stateful.common || key in this.stateful.states[this.stateful.currentState] || key in this.stateful.states[this.stateful.defaultState];
     },
     get (target, key) {
         const stateful = this.stateful;
         
-        // Attempt to find the property on the current or default state objects
-        // and if it's a function, change the scope and prefix the enter method
-        // argument.
-        if (key in stateful.states[stateful.currentState]) {
-            let result = stateful.states[stateful.currentState][key];
-            if (typeof result === 'function') {
-                return result.bind(stateful.scope || target, stateful.enter.bind(stateful));
-            }
-            return result;
-        }
-        else if (stateful.defaultState && key in stateful.states[stateful.defaultState]) {
-            let result = stateful.states[stateful.defaultState][key];
-            if (typeof result === 'function') {
-                return result.bind(stateful.scope || target, stateful.enter.bind(stateful));
-            }
-            return result;
+        // Functions need to be rebound to the stateful or the stateful's target scope!
+        function alter(fn) {
+            return typeof fn !== 'function' ? fn : fn.bind(stateful.scope || target, stateful.enter.bind(stateful));
         }
         
-        // Otherwise just return the base object's property unmodified.
+        // Highest priority: shared/common properties.
+        if (key in stateful.common) {
+            return alter(stateful.common[key]);
+        }
+        
+        // Second priority: current state properties.
+        if (key in stateful.states[stateful.currentState]) {
+            return alter(stateful.states[stateful.currentState][key]);
+        }
+        
+        // Third priority: default state properties.
+        else if (stateful.defaultState && key in stateful.states[stateful.defaultState]) {
+            return alter(stateful.states[stateful.defaultState][key]);
+        }
+        
+        // Lowest priority: base object properties.
         return target[key];
     },
     set (target, key, value) {
-        this.stateful.states[this.stateful.currentState][key] = value;
+        // Highest priority: shared/common properties
+        if (key in this.stateful.common) {
+            this.stateful.common[key] = value;
+        }
+        
+        // Otherwise: current state properties, even if they don't exist!
+        // NEVER modify the base object as this contradicts the design choice!
+        else {
+            this.stateful.states[this.stateful.currentState][key] = value;
+        }
     },
     deleteProperty (target, key) {
-        delete this.stateful.states[this.stateful.currentState][key];
+        // One does not delete shared/common properties. One can set them to undefined, tho...
+        if (key in this.stateful.common) {
+            this.stateful.common[key] = undefined;
+        }
+        else {
+            delete this.stateful.states[this.stateful.currentState][key];
+        }
     },
 };
 
